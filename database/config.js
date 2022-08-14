03d0443f4a9283f3847c1d5f850a08499863fe49
Rafael Velazco <rjvelazco21@gmail.com>
2022-08-14 13:16:13 -0400

    update: packages

--- database/config.js
+++ database/config.js
@@ -1,23 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const dbConnection = async() =>{
-
-    try{
-        await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('DB Online');
-    } catch(err){
-        console.log(err);
-        throw new Error('Error al conectar con la base de datos, ver logs');
-    }
-
-}
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Online");
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error al conectar con la base de datos, ver logs");
+  }
+};
 
 module.exports = {
-    dbConnection
-}
+  dbConnection,
+};
