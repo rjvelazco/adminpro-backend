3a8246e415203a056fa7446fe2b5a02671a72054
Rafael Velazco <rjvelazco21@gmail.com>
2022-08-14 14:40:01 -0400

    dev: re-build FrontEnd v2

--- public/main.js
+++ public/main.js
@@ -927,7 +927,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    base_url: 'http://localhost:8080/api'
+    base_url: 'https://angular-adminpro-adv.herokuapp.com/api'
 };
 /*
  * For easier debugging in development mode, you can import the following file
